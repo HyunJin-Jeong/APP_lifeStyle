505d5d46b1489bb8563439106ce5a77eadda4b97
hyunjin-jeong <hyun0524e@naver.com>
2020-09-05 21:43:55 +0900

    이름을 저장해 문구를 계속 띄워줌

--- greetings.js
+++ greetings.js
@@ -1,24 +1,47 @@
 const form = document.querySelector(".js-form"),
     input = form.querySelector("input"),
     greetings = document.querySelector(".js-greetings");
+
 const userName = "currentUser",
     showingClassName = "showing";
 
-function paintGreetings(text){
-    form.classList.remote(showingClassName);
-    greetings.classList.add(showingClassName);
-    greetings.innerText = `Hello ${text}`;
+// current value(userName)을 local Storage에 기억시키는 함수
+function saveName(text){
+    localStorage.setItem(userName, text);
 }
 
+// 입력받은 이름(current value) 값을 목적을 위한 함수로 보냄
+function handleSubmit(event){
+    event.preventDefault(); // Enter 시 새로고침되는 동작을 막는다(기본 동작)
+    const currentValue = input.value;
+    paintGreetings(currentValue);
+    saveName(currentValue);
+}
+
+// form을 통해 입력받으면 목적을 위한 함수로 보냄
+function askForName(){
+    form.classList.add(showingClassName);
+    form.addEventListener("submit", handleSubmit);
+}
+
+// 입력받은 이름으로 환영하는 문구를 띄움
+function paintGreetings(text){
+    form.classList.remove(showingClassName);
+    greetings.classList.add(showingClassName);
+    greetings.innerText = `Welcome ${text}!`;
+}
+
+// 기존에 입력받은 이름이 있는지 확인하는 함수
 function loadName(){
     const currentUser = localStorage.getItem(userName);
     if (currentUser == null){
-        // is not
+        askForName();
     } else {
         paintGreetings(currentUser);
     }
 }
 
+// initial
 function init(){
     loadName();
 }
